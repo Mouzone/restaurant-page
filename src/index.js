2ae94cbc573d694c97649b244e80f3b45a58036b
Mouzone <sunnyliu010@gmail.com>
2024-07-16 12:28:47 -0400

    Properly load content and link css to js in webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Deez")
+import './style.css'
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = 'Hello, dynamically created element!';
+    element.classList.add('content'); // Apply CSS class
+    return element;
+}
+
+document.body.appendChild(component());
