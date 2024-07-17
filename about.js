61c0223b7eaeb102f7b2af6d38c1cc7f93415766
Mouzone <sunnyliu010@gmail.com>
2024-07-17 12:14:26 -0400

    Write content for about tab

--- about.js
+++ about.js
@@ -1,4 +1,11 @@
 export function generateAbout(){
     const content = document.querySelector("#content")
 
+    const heading = document.createElement("h1")
+    heading.textContent = "Our Story"
+    content.appendChild(heading)
+
+    const story = document.createElement("p")
+    story.textContent = "Positano Pizza & Ristaurante was est. February 1993. It started with a boy who lived in Naples, Italy. Tony started as a dishwasher working his way up in local restaurants and the Amalfi Coast. His greatest memories were when he worked in a town called Positano. Growing a deep passion for food, he came to the U.S. to have the American dream to own his own restaurant. His dream became reality and now Tony owns a family operated business. Local visitors coming from New Hyde Park, Mineola, Williston Park, Garden City & Manhasset to get a taste of Positano’s great food."
+    content.appendChild(story)
 }
